264b3012987d6eda12376ce5d4b0ce789aaa64f2
Leticia BuCor <cursocompartido@gmail.com>
2022-08-26 13:39:52 -0500

    Se anexa IClonable

---
+++ PI_04ICloneable/CAuto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_04ICloneable
+{
+    class CAuto:IAutomovil ,ICloneable
+    {
+        double costo;
+        double tenencia;
+        string modelo;
+        double impuesto;
+        public CAuto(string pModelo, double pCosto)
+        {
+            modelo = pModelo;
+            costo = pCosto;
+        }
+
+        public string Modelo { get; set; } = "BMW";
+        public double Costo
+        {
+            get => costo;
+            set => costo = value;
+        }
+        public double Tenencia
+        {
+            get { return tenencia; }
+            set { tenencia = value; }
+        }
+
+      
+        public void CalcularTenencia(double pImpuesto)
+        {
+            tenencia = 5000 + costo + pImpuesto;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("Automovil {0}, tiene un costo de {1}, y tenencia de {2}", modelo, costo, tenencia);
+            Console.WriteLine("Total apagar {0}", costo + tenencia);
+            Console.WriteLine("----");
+        }
+
+        //Se implementa el método de Clone(), de la interfaz Icloneable para poder usarlo
+        public Object Clone()
+        {
+            CAuto auto = new CAuto(modelo, costo);
+            auto.tenencia = tenencia;
+            return auto;
+        }
+    }
+}
---
+++ PI_04ICloneable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_04ICloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Una clonación de un objeto que después de clonarse sea un objeto independiente, se usa 
+              clone de la interfaz ICloneable, para esto hay eque implentar su método,
+              cuando se hiciera
+                CAuto auto1 = new CAuto("MWX", 300);
+                CAuto auto2;
+                CAuto auto1= auto2; Esto no es una clonación ya que auto2, estaría referenciandose a auto1
+                                    //Lo que significa que lo que se haga en auto1, se reflejaria en auto2 y
+                                    //vicevesa
+                
+             */
+
+            CAuto auto1 = new CAuto("MWX", 300);
+
+            auto1.CalcularTenencia(500);
+            auto1.Costo = 0.5;           
+            auto1.MostrarInformacion();
+
+            Console.WriteLine("-----");
+
+            CAuto auto2 =(CAuto)auto1.Clone();
+            auto1.MostrarInformacion();
+            auto2.MostrarInformacion();
+
+            Console.WriteLine("-----");
+
+            auto1.Costo=20;
+
+            auto1.MostrarInformacion();
+            auto2.MostrarInformacion();
+
+            Console.WriteLine("----");
+            Console.ReadLine();
+        }
+    }
+}
