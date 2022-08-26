575ac117c2cdf866748a5e35471949f5e9a42fcc
Leticia BuCor <cursocompartido@gmail.com>
2022-08-26 14:55:50 -0500

    Se anexa ICloneable  por medio de implementar el método CompareTo de la Interfaz IClonable nos permite definir nuestro propio ordenamiento y tener acceso al méto Array.Sort

---
+++ PI_06IComparable/CRectangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_06IComparable
+{
+    class CRectangulo :IComparable
+    {
+        private double ancho;
+        private double alto;
+        private double area;
+
+        public CRectangulo(double pAncho, double pAlto)
+        {
+            ancho = pAncho;
+            alto = pAlto;
+            CalcularArea();
+        }
+
+        private void CalcularArea()
+        {
+            area = ancho * alto;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ancho {0}, alto {1}= {2}", ancho, alto, area);
+        }
+
+        //Implementación de IComparable
+        public int CompareTo(object obj)
+        {
+            CRectangulo tempRectangulo = (CRectangulo)obj;
+            if (area > tempRectangulo.area) return 1;
+            else if (area < tempRectangulo.area) return -1;
+            else return 0;
+        }
+    }
+}
---
+++ PI_06IComparable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_06IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CRectangulo[] rectangulo = new CRectangulo[5];
+            rectangulo[0] = new CRectangulo(2, 1);
+            rectangulo[1] = new CRectangulo(4, 5);
+            rectangulo[2] = new CRectangulo(5, 7);
+            rectangulo[3] = new CRectangulo(1, 5);
+            rectangulo[4] = new CRectangulo(2, 4);
+
+            foreach(CRectangulo item in rectangulo)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("-----Muestra arreglo ordenado asc--------");
+
+            Array.Sort(rectangulo);//Una vez implementado el metodo IComparable ya se puede usar el Método Sort
+
+            foreach (CRectangulo item in rectangulo)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("-----Muesta arreglo ordenado desc----------");
+
+            for ( int i=rectangulo.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(rectangulo[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
