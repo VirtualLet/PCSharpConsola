543bb85b1b66935846ac2db2631261820b0dc32c
Leticia BuCor <cursocompartido@gmail.com>
2022-08-30 20:36:41 -0500

    Se agrega ejemplo de uso de propiedades

---
+++ POO_07Propiedades/CEmpleado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_07Propiedades
+{
+    class CEmpleado
+    {
+        private float sueldo;
+        private float impuesto;
+        private float resultado;
+
+        public CEmpleado()
+        {
+            impuesto = 1.15f;
+        }
+        public float Sueldo
+        {
+            get => sueldo; 
+            set
+            {
+                if (value > 10000)
+                    sueldo = value;
+                else
+                    sueldo = 10000;                
+            }
+        }
+
+        private float Impuesto { get => impuesto; }        
+     
+        public void CalcularImpuesto() => resultado = sueldo + impuesto;        
+
+        public string Mostrar()
+        {
+            return string.Format("Sueldo: {0}, Impuesto:{1}, SueldoBruto:{2}", Sueldo, Impuesto, resultado);
+        }
+    }
+}
---
+++ POO_07Propiedades/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_07Propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEmpleado empleado = new CEmpleado();
+            empleado.Sueldo = 50000;
+            empleado.CalcularImpuesto();
+            Console.WriteLine(empleado.Mostrar());
+            Console.ReadLine();
+        }
+    }
+}
