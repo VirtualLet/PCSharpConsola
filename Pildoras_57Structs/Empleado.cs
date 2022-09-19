ddbd96d68ae155c6743b11efd0b98dca3814d36a
Leticia BuCor <cursocompartido@gmail.com>
2022-09-19 18:34:09 -0500

    Sube cambios Structs

---
+++ Pildoras_57Structs/Empleado.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pildoras_57Structs
+{
+   public struct Empleado
+    {
+       string nombre;
+        double salario;
+        double comision;
+        
+        public Empleado(string pNombre,double pSalarioBase, double pComision)
+        {
+            nombre = pNombre;
+            salario = pSalarioBase;          
+            comision = pComision;
+        }
+
+        private double EvaluarSalario(double pSalario)
+        {
+            if (pSalario < 0) return 0;
+            else return pSalario;
+        }
+
+
+        public double Salario
+        {
+            get => salario;
+            set => salario = EvaluarSalario(value);
+        }
+
+        public void CambiarSalario(double pIncremento)
+        {
+            Salario += pIncremento;
+            comision += pIncremento;
+        }
+
+        //Al intentar enviarlo por referencia a la clase cuando se usa struct no se reflejará cambio
+        public void CambiarSalario(Empleado pEmp, double pIncremento)
+        {
+            pEmp.salario += pIncremento;
+            pEmp.comision += pIncremento;
+        }
+
+        public override string ToString()
+        {
+       
+          return string.Format("El salario de {2},  es de {0} y la comisión es {1}", EvaluarSalario(salario),comision,nombre);
+
+        }
+    }
+}
--- Pildoras_57Structs/Program.cs
+++ Pildoras_57Structs/Program.cs
@@ -6,7 +6,32 @@ namespace Pildoras_57Structs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Probando Struct!");
+            /* No permite la declaración de constructor por defecto
+             * El compilador no inicia los campos. Puedes iniciarlos en el constructor.
+             * No puede haber sobrecarga de constructores
+             * No heredan de otras clases pero si implementan interfaces
+             * Son selladas (sealed)
+             */
+
+            Empleado empleado = new Empleado("Juan", 1000, 400);
+            Console.WriteLine(empleado);
+            //En este caso el valor del incremento no se vera reflejado ya que al ser un struct y no una 
+            //clase el cambio lo realiza en la copia y no en la instancia original ya que struct
+            //usa valor(stack) a diferencia de la clase que utiliza Referencia(heap)
+            empleado.CambiarSalario(empleado, 5);
+            Console.WriteLine(empleado);
+            Console.WriteLine("----------");
+            //para hacer el cambio se tiene que especificar de otra manera pero no por referencia
+            empleado.CambiarSalario(4);
+            Console.WriteLine(empleado);
+            empleado.CambiarSalario(10);
+            Console.WriteLine(empleado);
+
+            Empleado empleado1 = new Empleado("Teresa", 200, 100);
+            empleado1.CambiarSalario(2);
+            Console.WriteLine(empleado1);
+            Console.ReadLine();
         }
     }
 }
