1bf025472bb766cfccf90e7eb2f99cfd0baf4b8d
Leticia BuCor <cursocompartido@gmail.com>
2022-09-30 15:03:29 -0500

    sube indivitual 67Delegado

---
+++ Pildoras_67Delegado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pildoras_67Delegado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
